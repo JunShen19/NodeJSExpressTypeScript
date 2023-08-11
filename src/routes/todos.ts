588443fbe10ffb02593000889f9c65187db7ff96
JunShen <tony88010955@gmail.com>
2023-08-11 16:37:41 +0800

    finsih

---
+++ src/routes/todos.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  createTodo,
+  deleteTodo,
+  getTodo,
+  updateTodo,
+} from "../controllers/todos";
+const router = Router();
+
+router.post("/", createTodo);
+
+router.get("/", getTodo);
+
+router.patch("/:id", updateTodo);
+
+router.delete("/:id", deleteTodo);
+
+export default router;
